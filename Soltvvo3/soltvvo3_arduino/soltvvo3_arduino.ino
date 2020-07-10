a4d60d6c5b68309c85bd697d7c680f2825a41b9e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-07-11 00:52:52 +0900

    test

--- Soltvvo3/soltvvo3_arduino/soltvvo3_arduino.ino
+++ Soltvvo3/soltvvo3_arduino/soltvvo3_arduino.ino
@@ -31,21 +31,21 @@ void move_motor(long num, long deg, long spd) {
   for (int i = 0; i < accel; i++) {
     motor_hl = !motor_hl;
     digitalWrite(step_pul[num], motor_hl);
-    if (analogRead(sensor[num]) < 1000)
+    if (analogRead(sensor[num1]) < 1000)
       digitalWrite(step_pul[num1], hl);
     delayMicroseconds(max_time - slope * i);
   }
   for (int i = 0; i < steps * 2 - accel * 2; i++) {
     motor_hl = !motor_hl;
     digitalWrite(step_pul[num], motor_hl);
-    if (analogRead(sensor[num]) < 1000)
+    if (analogRead(sensor[num1]) < 1000)
       digitalWrite(step_pul[num1], hl);
     delayMicroseconds(avg_time);
   }
   for (int i = 0; i < accel; i++) {
     motor_hl = !motor_hl;
     digitalWrite(step_pul[num], motor_hl);
-    if (analogRead(sensor[num]) < 1000)
+    if (analogRead(sensor[num1]) < 1000)
       digitalWrite(step_pul[num1], hl);
     delayMicroseconds(max_time - slope * accel + accel * (i + 1));
   }
@@ -87,6 +87,7 @@ void setup() {
 }
 
 void loop() {
+  /*
   if (Serial.available()) {
     buf[idx] = Serial.read();
     if (buf[idx] == '\n') {
@@ -103,4 +104,8 @@ void loop() {
       idx++;
     }
   }
+  */
+  Serial.print(analogRead(sensor[0]));
+  Serial.print("\t");
+  Serial.println(analogRead(sensor[1]));
 }
