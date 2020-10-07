857953b6afe5c5d2977d2a8f7641bb0c07ece27f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-10-07 11:18:35 +0900

    idx done

--- Soltvvo3.2/create_array.py
+++ Soltvvo3.2/create_array.py
@@ -29,34 +29,48 @@ def move_co(co, arr):
 # Return the number of CP
 def cp2idx(cp):
     res = 0
-    for i in range(4):
-        cnt = 0
+    for i in range(8):
+        cnt = cp[i]
         for j in cp[:i]:
             if j < cp[i]:
-                cnt += 1
-        res += fac[7 - i] * (cp[i] - cnt)
-    for i in range(4, 8):
-        cnt = cp[i]
-        for j in cp[i + 1:]:
-            if j < cp[i]:
                 cnt -= 1
-        res += fac[7 - i] * (cp[i] - cnt)
+        res += fac[7 - i] * cnt
     return res
 
 # co配列から固有の番号を作成
 # Return the number of CO
 def co2idx(co):
     res = 0
-    for i in co:
+    for i in co[:7]:
         res *= 3
         res += i
     return res
 
 def idx2cp(cp_idx):
     res = [-1 for _ in range(8)]
+    for i in range(8):
+        candidate = cp_idx // fac[7 - i]
+        marked = [True for _ in range(i)]
+        for _ in range(8):
+            for j, k in enumerate(res[:i]):
+                if k <= candidate and marked[j]:
+                    candidate += 1
+                    marked[j] = False
+        res[i] = candidate
+        cp_idx %= fac[7 - i]
+    return res
+
+def idx2co(co_idx):
+    res = [0 for _ in range(8)]
+    for i in range(7):
+        res[6 - i] = co_idx % 3
+        co_idx //= 3
+    res[7] = (3 - sum(res) % 3) % 3
+    return res
 
 fac = [1]
-for i in range(1, 8):
+for i in range(1, 9):
     fac.append(fac[-1] * i)
 
-cp_cost = [-1 for _ in range(fac[7])]
+cp_cost = [-1 for _ in range(fac[8] * 9)]
+
