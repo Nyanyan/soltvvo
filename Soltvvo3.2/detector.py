f142f613c2d6fba6584c7b0bb6b113297eeac949
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2023-05-16 21:32:28 +0900

    updating color detector with nn-like method

--- Soltvvo3.2/detector.py
+++ Soltvvo3.2/detector.py
@@ -49,8 +49,9 @@ def detector():
         # for normal sticker
         #color_low = [[50, 50, 50],   [90, 50, 50],    [160, 70, 50],  [170, 20, 50],  [20, 50, 50],   [0, 0, 50]]
         #color_hgh = [[90, 255, 255], [140, 255, 255], [10, 255, 200], [20, 255, 255], [50, 255, 255], [179, 50, 255]]
-        color_low = [[40, 50, 50],   [90, 50, 70],    [160, 50, 50],   [170, 50, 50],    [20, 50, 30],   [0, 0, 50]]
-        color_hgh = [[90, 255, 255], [140, 255, 200], [170, 255, 255], [10, 255, 255], [40, 255, 255], [179, 50, 255]]
+        #color_low = [[40, 50, 50],   [90, 50, 70],    [160, 50, 50],   [170, 50, 50],    [20, 50, 30],   [0, 0, 50]]
+        #color_hgh = [[90, 255, 255], [140, 255, 200], [170, 255, 255], [10, 255, 255], [40, 255, 255], [179, 50, 255]]
+        color_center = [(0, 255, 0), (255, 0, 0), (0, 0, 255), (0, 170, 255), (0, 255, 255), (255, 255, 255)]
         circlecolor = [(0, 255, 0), (255, 0, 0), (0, 0, 255), (0, 170, 255), (0, 255, 255), (255, 255, 255)]
         surfacenum = [[[4, 2], [4, 3], [5, 2], [5, 3]], [[2, 2], [2, 3], [3, 2], [3, 3]], [[0, 2], [0, 3], [1, 2], [1, 3]], [[3, 7], [3, 6], [2, 7], [2, 6]]]
         for _ in range(5):
@@ -62,32 +63,29 @@ def detector():
         tmp_colors = [['' for _ in range(8)] for _ in range(6)]
         dx = [-1, -1, 1, 1]
         dy = [-1, 1, -1, 1]
-        loopflag = [1 for _ in range(4)]
-        while sum(loopflag):
-            ret, show_frame = capture.read()
-            show_frame = cv2.resize(show_frame, (size_x, size_y))
-            #cv2.imshow('title',show_frame)
-            hsv = cv2.cvtColor(show_frame,cv2.COLOR_BGR2HSV)
-            for i in range(4):
-                y = center[0] + dy[i] * d
-                x = center[1] + dx[i] * d
-                cv2.circle(show_frame, (y, x), 5, (0, 0, 0), thickness=3, lineType=cv2.LINE_8, shift=0)
-                val = hsv[x, y]
-                for j in range(6):
-                    flag = True
-                    for k in range(3):
-                        if not ((color_low[j][k] < color_hgh[j][k] and color_low[j][k] <= val[k] <= color_hgh[j][k]) or (color_low[j][k] > color_hgh[j][k] and (color_low[j][k] <= val[k] or val[k] <= color_hgh[j][k]))):
-                            flag = False
-                    if flag:
-                        tmp_colors[surfacenum[idx][i][0]][surfacenum[idx][i][1]] = j2color[j]
-                        cv2.circle(show_frame, (y, x), 15, circlecolor[j], thickness=3, lineType=cv2.LINE_8, shift=0)
-                        cv2.circle(show_frame, (y, x), 20, (0, 0, 0), thickness=2, lineType=cv2.LINE_8, shift=0)
-                        loopflag[i] = 0
-                        break
+        ret, show_frame = capture.read()
+        show_frame = cv2.resize(show_frame, (size_x, size_y))
         #cv2.imshow('title',show_frame)
-        #if cv2.waitKey(0) == 32: #スペースキーが押されたとき When space key pressed
+        #hsv = cv2.cvtColor(show_frame,cv2.COLOR_BGR2HSV)
         for i in range(4):
-            colors[surfacenum[idx][i][0]][surfacenum[idx][i][1]] = tmp_colors[surfacenum[idx][i][0]][surfacenum[idx][i][1]]
+            y = center[0] + dy[i] * d
+            x = center[1] + dx[i] * d
+            cv2.circle(show_frame, (y, x), 5, (0, 0, 0), thickness=3, lineType=cv2.LINE_8, shift=0)
+            val = show_frame[x, y]
+            print(i, y, x, val)
+            min_dis = 100000000
+            min_color_idx = 0
+            for j in range(6):
+                dis = 0
+                for k in range(3):
+                    dis += (color_center[j][k] - val[k]) ** 2
+                if dis < min_dis:
+                    min_dis = dis
+                    min_color_idx = j
+            colors[surfacenum[idx][i][0]][surfacenum[idx][i][1]] = j2color[min_color_idx]
+            cv2.circle(show_frame, (y, x), 15, circlecolor[min_color_idx], thickness=3, lineType=cv2.LINE_8, shift=0)
+            cv2.circle(show_frame, (y, x), 20, (0, 0, 0), thickness=2, lineType=cv2.LINE_8, shift=0)
+        cv2.imshow('title',show_frame)
         colors = fill(colors)
         move_actuator(0, 0, -90, rpm)
         move_actuator(1, 0, 90, rpm)
